9e4db5bc3bd376bf0cc8a2ad971bd25c869674bc
SayanIndra83 <sayanindra0@gmail.com>
2026-04-12 00:20:26 +0530

    Mailling bugs fixed

--- ai-ticket-backend/utils/mailer.js
+++ ai-ticket-backend/utils/mailer.js
@@ -6,7 +6,9 @@ dns.setDefaultResultOrder('ipv4first');
 export const sendMail = async (to, subject, text) => {
     try {
         const transporter = nodemailer.createTransport({
-        service: "smtp.gmail.com",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
         auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
